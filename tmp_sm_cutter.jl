e38dae1788dc0b07998bbc2e95f580f1b91bed39
Pere Antoni Martorell <pmartorell@cimne.upc.edu>
2020-04-09 17:35:08 +0200

    Implementing comformal surface mesh cutter [ci skip]

--- tmp_sm_cutter.jl
+++ tmp_sm_cutter.jl
@@ -1,8 +1,8 @@
 module sm_cutter
 
 using STLCutter
-using STLCutter: compute_surface_mesh_face_to_cells, get_vertex_coordinates, get_cell_coordinates, UNSET
-
+using STLCutter: compute_surface_mesh_face_to_cells, get_vertex_coordinates, get_cell_coordinates, get_dface_to_vertices, UNSET
+import STLCutter: closest_point, distance
 
 function find_closest_background_face(bg_mesh::VolumeMesh,point::Point,sm_face_to_bg_cells::Table,vertex::Integer)
   min_dist = tol()
@@ -36,6 +36,241 @@ function find_closest_background_face(bg_mesh::VolumeMesh,point::Point,sm_face_t
   @assert false
 end
 
+function cells_containg_nfaces!(
+  cells::Vector,
+  m::VolumeMesh,
+  sm::SurfaceMesh,
+  sm_d::Integer,
+  sm_face::Integer,
+  n::Integer,
+  sm_vertex_to_d::Vector,
+  sm_vertex_to_dface::Vector)
+
+  resize!(cells,0)
+  D = num_dims(m)
+  sm_df_to_sm_nf = get_faces(sm,sm_d,n)
+  sm_nf_to_sm_v = get_dface_to_vertices(sm,n)
+  for lnface in 1:length( sm_df_to_sm_nf, sm_d )
+    sm_nface = sm_df_to_sm_nf[ sm_face, lnface ]
+    for lvertex in 1:length( sm_nf_to_sm_v, sm_nface )
+      sm_vertex = sm_nf_to_sm_v[ sm_nface, lvertex ]
+      dface = sm_vertex_to_dface[ sm_vertex ]
+      d = sm_vertex_to_d[ sm_vertex ] 
+      if dface != UNSET
+        df_to_c = get_faces(m,d,D)
+        for i in 1:length(df_to_c,dface)
+          cell = df_to_c[dface,i]
+          if cell ∉ cells
+            push!(cells,cell)
+          end
+        end
+      end
+    end
+  end
+  cells
+end
+
+function fetch_boundary_nfaces!(
+  nfaces::Vector,
+  d_to_dface_to_vertices::Vector{<:Table},n::Integer,
+  m::VolumeMesh,bg_cell::Integer,
+  vertex_to_bg_d::Vector,vertex_to_bg_dface::Vector)
+
+  resize!(nfaces,0)
+  nf_to_v = d_to_dface_to_vertices[n+1]
+  for nface in 1:length(nf_to_v)
+    if is_nface_in_bg_cell(nf_to_v,nface,m,bg_cell,vertex_to_bg_d,vertex_to_bg_dface)
+      push!(nfaces,nface)
+    end
+  end
+  nfaces
+end
+
+function is_nface_in_bg_cell(
+  nface_to_vertices::Table,nface::Integer,
+  m::VolumeMesh,bg_cell::Integer,
+  vertex_to_bg_d::Vector,
+  vertex_to_bg_dface::Vector)
+
+  D = num_dims(m)
+  for lvertex in 1:length(nface_to_vertices,nface)
+    vertex = nface_to_vertices[nface,lvertex]
+    bg_d = vertex_to_bg_d[vertex]
+    bg_dface = vertex_to_bg_dface[vertex]
+    bg_c_to_bg_df = get_faces(m,D,bg_d)
+    for ldface in 1:length(bg_c_to_bg_df,bg_cell)
+      _bg_dface = bg_c_to_bg_df[bg_cell,ldface]
+      if _bg_dface == bg_dface
+        return true
+      end
+    end
+  end
+  false
+end
+
+#function complete_boundary( )
+#  sm_d
+#  n
+#
+#  vertices = get_vertices(nfaces)
+#
+#  while length(vertices) > 0
+#    vertex = pop!(vertices)
+#    bg_dface = vertex_to_bg_d
+#    bg_d = vertex_to_bg_dface
+#    _find_next_intersection()
+#  end
+#  
+#
+#end
+#
+#
+#function _find_next_intersection(bg_cell...)
+#  min_dist = tol()
+#  closest_bg_face = UNSET
+#
+#  c_to_f = get_faces(m,D,D-1)
+#  for d in 0:D-sm_d
+#    f_to_df = get_faces(m,D,D-1)
+#    for lfacet in 1:length(c_to_f,bg_cell)
+#      bg_facet = c_to_f[bg_cell,lfacet]
+#
+#      if sm_d == 1 || is_around(m,facet,bg_face,bg_d) 
+#        for ldface in 1:lenth(f_to_df,bg_facet)
+#          _bg_dface = f_to_df[bg_facet,ldface]
+#
+#          if contains_any_nface( vertices, d, _bg_dface) 
+#            
+#            dist = distance(m,d,_bg_dface,sm,sm_d,sm_dface)
+#          if dist < min_dist
+#            min_dist = dist
+#            closest_bg_face = _bg_dface
+#          end
+#        end
+#      end
+#    end
+#    if closest_bg_face != UNSET
+#      _d = d
+#      break
+#    end
+#  end
+#
+#  if closest_bg_face != UNSET
+#    point = closest_point(bg_m,_d,closest_bg_face,sm,sm_d,sm_dface)
+#    return closest_bg_face,_d,point
+#  end
+#  
+#  return UNSET,UNSET, zero( eltype(get_vertex_coordinates(m)) )
+#end
+#
+#function get_vertices(nfaces)
+#  vertices = Int[]
+#  for nface in nfaces
+#    for lvertex in 1:length(nface_to_vertices,nface)
+#      vertex = nface_to_vertices[nface,lvertex]
+#      if vertex ∉ vertices
+#        push!(vertices,vertex)
+#      end
+#    end
+#  end
+#  vertices
+#end
+
+@generated function distance(
+  m::VolumeMesh{D},d::Integer,dface::Integer,
+  sm::SurfaceMesh{D},n::Integer,sm_nface::Integer) where D
+
+  msg1 = "\$d-face does not exist at \$(typeof(m))"
+  msg2 = "\$n-face does not exist at \$(typeof(sm))"
+  msg3 = "distance() from \$d-face in \$(typeof(m)) against \$n-face in \$(typeof(sm)) not implemented"
+  
+  body = ""
+  for d in 0:D
+    sm_body = ""
+    body *= "if d == $d \n"
+    body *= "  f$d = get_face_coordinates(m,Val{$d}(),dface) \n"
+    for n in 0:min(D-d,D-1)
+      sm_body *= "if n == $n \n"
+      sm_body *= "    sm_f$n = get_face_coordinates(sm,Val{$n}(),sm_nface) \n"
+      sm_body *= "    distance(f$d,sm_f$n)\n  "
+      sm_body *= "else"
+    end
+    error = "if n > $(D-1) \n"
+    error *= "    throw(ArgumentError(\"$msg2\"))\n  "
+    error *= "else \n"
+    error *= "    throw(ArgumentError(\"$msg3\")) \n  "
+    error *= "end"
+    condition = sm_body * error
+    body *= "  $condition \n"
+    body *= "else"
+  end
+  error = "  throw(ArgumentError(\"$msg1\"))\n"
+  str = body * '\n' * error * "end"
+
+  Meta.parse(str)
+end
+
+@generated function closest_point(
+  m::VolumeMesh{D},d::Integer,dface::Integer,
+  sm::SurfaceMesh{D},n::Integer,sm_nface::Integer) where D
+
+  msg1 = "\$d-face does not exist at \$(typeof(m))"
+  msg2 = "\$n-face does not exist at \$(typeof(sm))"
+  msg3 = "closest_point() from \$d-face in \$(typeof(m)) against \$n-face in \$(typeof(sm)) not implemented"
+  
+  body = ""
+  for d in 0:D
+    sm_body = ""
+    body *= "if d == $d \n"
+    body *= "  f$d = get_face_coordinates(m,Val{$d}(),dface) \n"
+    for n in 0:min(D-d,D-1)
+      sm_body *= "if n == $n \n"
+      sm_body *= "    sm_f$n = get_face_coordinates(sm,Val{$n}(),sm_nface) \n"
+      sm_body *= "    closest_point(f$d,sm_f$n)\n  "
+      sm_body *= "else"
+    end
+    error = "if n > $(D-1) \n"
+    error *= "    throw(ArgumentError(\"$msg2\"))\n  "
+    error *= "else \n"
+    error *= "    throw(ArgumentError(\"$msg3\")) \n  "
+    error *= "end"
+    condition = sm_body * error
+    body *= "  $condition \n"
+    body *= "else"
+  end
+  error = "  throw(ArgumentError(\"$msg1\"))\n"
+  str = body * '\n' * error * "end"
+
+  Meta.parse(str)
+end
+
+
+struct IncrementalSurfaceMesh{D,T}
+  vertex_coordinates::Vector{Point{D,T}}
+  d_to_dface_to_vertices::Vector{Table{Int}}
+  d_to_dface_to_sm_n::Vector{Vector{Int8}}
+  d_to_dface_to_sm_nface::Vector{Vector{Int}}
+  vertex_to_bg_d::Vector{Int8}
+  vertex_to_bg_dface::Vector{Int}
+end
+
+function IncrementalSurfaceMesh{D,T}() where {D,T}
+  vertex_coordinates = Point{D,T}[]
+  d_to_dface_to_vertices = [ Table{Int}() for d in 0:D-1 ]
+  d_to_dface_to_sm_n = [ Int8[] for d in 0:D-1 ]
+  d_to_dface_to_sm_nface = [ Int[] for d in 0:D-1 ]
+  vertex_to_bg_d = Int8[]
+  vertex_to_bg_dface = Int[]
+  IncrementalSurfaceMesh(
+    vertex_coordinates,
+    d_to_dface_to_vertices,
+    d_to_dface_to_sm_n,
+    d_to_dface_to_sm_nface,
+    vertex_to_bg_d,
+    vertex_to_bg_dface )
+end
+
+
 tol() = 1e-10
 
 points = [
@@ -65,6 +300,7 @@ display(sm_face_to_bg_cells)
 point_type = eltype( get_vertex_coordinates(sm) )
 D = num_dims(sm)
 
+
 vertex_coordinates = point_type[]
 d_to_dface_to_vertices = [ Table{Int}() for d in 0:D-1 ]
 d_to_dface_to_sm_n = [ Int[] for d in 0:D-1 ]
@@ -73,6 +309,10 @@ d_to_dface_to_sm_nface = [ Int[] for d in 0:D-1 ]
 vertex_to_bg_d = Int[]
 vertex_to_bg_dface = Int[]
 
+bg_cells = Int[]
+nfaces = Int[]
+
+
 for vertex in 1:num_vertices(sm)
   point = get_vertex_coordinates(sm,vertex)
   _d, bg_face = find_closest_background_face( vm, point, sm_face_to_bg_cells, vertex )
@@ -80,46 +320,29 @@ for vertex in 1:num_vertices(sm)
   push!( vertex_coordinates, point )
   push!( vertex_to_bg_dface, bg_face )
   push!( vertex_to_bg_d, _d )
+  push!( d_to_dface_to_vertices[0+1], [vertex] )
+  push!( d_to_dface_to_sm_n[0+1], 0 )
+  push!( d_to_dface_to_sm_nface[0+1], vertex )
 end
 
 @show vertex_to_bg_d
 
 
-
-#for d in 1:D-1
-#  for SMFace in faces(d,surface_mesh)
-#    
-#    n = d-1
-#    bg_cells = cells_containg_nfaces( bg_mesh, _sm, n )
-#    
-#    while length(bg_cells) > 0
-#      bg_cell = pop!(bg_cells)
-#        
-#      ## 1) Fetch sm_face-bg_cell intersection already computed
-#      ##    ( ∂Face_SM ∩ Cell_BgM ) ∩ ( 2) of neighbor cells )
-#
-#      nfaces = fetch_boundary_nfaces( bg_cell, _sm, n )
-#
-#      ## 2) Complete intersections in cell boundary, to define a close domain
-#      ##    Face_SM ∩ ∂Cell_BgM 
-#      
-#      append!(nfaces, complete_boundary(bg_cell,_sm,SMFace) )
-#
-#      ## 3) Now we have a convex domain on n-dim space bounded by nfaces
-#      ##    Create symplex mesh locally (dface = nface ∪ vertex):
-#
-#      add_faces!( sm, nfaces, n )
-#      
-#      ## *) Feed depth-first traversal
-#
-#      for _bg_cell in cells_touching( bg_mesh, nfaces ) #new nfaces actually, what is computed in 2)
-#        push!( bg_cells, _bg_cell )
-#      end
-#
-#    end
-#  end
-#end
-
-
+for d in 1:D-1
+  for sm_dface in 1:num_faces(sm,d)
+    n = d-1
+    cells_containg_nfaces!(
+      bg_cells,vm,
+      sm,d,sm_dface,n,
+      vertex_to_bg_d,vertex_to_bg_dface)
+    @show bg_cells
+    while length(bg_cells) > 0
+      bg_cell = pop!(bg_cells)
+      fetch_boundary_nfaces!(
+        nfaces,d_to_dface_to_vertices,n,vm,bg_cell,vertex_to_bg_d,vertex_to_bg_dface)
+      @show nfaces
+    end
+  end
+end
 
 end # module
