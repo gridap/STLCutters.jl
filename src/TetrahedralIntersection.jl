4219f8108696f4a521a62bfc8f5b30fe6b0f789a
Santiago Badia <santiago.badia@monash.edu>
2020-10-29 13:12:27 +1100

    comments on the final alg

--- src/TetrahedralIntersection.jl
+++ src/TetrahedralIntersection.jl
@@ -1,3 +1,64 @@
+# I propose a two-fold algorithm:
+
+# Part 1. Vertex insertion using a binary tree. As a result of this step we have
+# a hybrid tree that involves a first level, the original mesh T, which can be
+# represented as a structured mesh, an octree, etc. At each cell of this mesh,
+# we create a binary tree (if the cell includes a vertex in its interior). In this
+# step, we must create a vertex local indexing (at each cell in T) that will
+# be suitable for nonconforming meshes, using the concept of slave and master
+# vertices. As a result of this step, each cell in T that contains one vertex of
+# the STL returns a new tree mesh TR. TR is simplexified in order to create S0.
+
+# Part 2. For each cell s in SR (for each cell in T) we insert edges. There are
+# many different ways to insert an edge. An edge is shared by two faces. One
+# approach is to consider the plane that is the bisection of these two planes.
+# Another approach (with a sligth reduction in the number of cells) is to take!
+# one vertex of s and cut it by the plane that contains the edge and the vertex.
+# There are multiple vertices, so we could define a way to get a _well-posed_
+# one. On the other hand, the newly created ones can be freely moved if close
+# to cell vertices and eliminate the cells freely. Edge insertion can be performed
+# exactly as above but taking edges instead of planes and defined the bisection
+# plane of an edge. After this process, we have a new mesh S1.
+
+# Part 3. Cut s against the facets (or planes, since no edges) using the algorithm
+# below, to SF.
+
+# Some discussion:
+
+# I note that in the previous
+# discussions, we were considering a step for edges (via cut planes) and another
+# step for faces. However, when the cut plane for the edge contains a face, we
+# had issues that we tried to solve using hacks. This is not going to happen
+# taking as the plane the bisection plane.
+
+# Another option, radically different, is to insert edges using a plane
+# determined by one of the STL facets that
+# contain that edge. So we know that we are always in the _ill_ situation above.
+# In fact, doing that, we reduce the number of cuts, since we reuse the edge
+# insertion and one facet insertion and all the other edges on that face.
+
+# But at the end of the day, doing this is just inserting the facets. Which is the
+# problem with this? The plane can also be perturbed as above, since we are not
+# really touching the STL but the cell / STL intersection. But we should modify
+# the plane (locally) to make this change _exact_.
+
+# The next part is IN/OUT.
+
+# I have to remember
+# how it is being finally done. In any case, we could consider two different
+# strategies. One option is a ray-shooting technique (as in Fenics article).
+# Another option is the following. Since you know which are the planes
+# facets of the cells in SF are on, you can
+# just check (using coordinates) whether the midpoint of the facet is on the cell
+# facet (potentially after modification). If they are, you can mark IN/OUT. If none is on the real STL boundary,
+# we cannot mark the vertices through this cell.
+
+# If a vertex receives in and out from different cells, we should re-run Part 2
+# with more precision. Only if we observe that this is really needed. Since we
+# are not going to use flat cells for IN/OUT (unless absolutely necessary) it is
+# probably not that problematic. In any case, this part has to be elaborated if
+# needed.
+
 # Pseudo-code to perform tetrahedral intersections
 # The following code should be called for each cell obtained after
 # simplexify the kd-tree cells that have intersection with planes
