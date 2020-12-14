7b41b3cae112d4c24c7cab580972635f25636a6f
Pere Antoni Martorell <pmartorell@cimne.upc.edu>
2020-12-14 08:27:25 +0100

    Fix CI fail

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,6 @@ using Test
 @testset "STLs" begin include("STLsTests.jl") end
 @testset "Intersections" begin include("IntersectionsTests.jl") end
 @testset "Polyhedra" begin include("PolyhedraTests.jl") end
+@testset "Rotations" begin include("Rotations.jl") end
 
 end # module
