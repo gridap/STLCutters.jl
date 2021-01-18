51887936ef73eb97c9e9ed48d9e9374ec3b86052
Pere Antoni Martorell <pmartorell@cimne.upc.edu>
2021-01-18 17:38:45 +0100

    Added test matrix

--- test/TestMatrix.jl
+++ test/TestMatrix.jl
@@ -1,5 +1,5 @@
-module Rotations
-  
+module TestMatrix
+
 using Test
 
 using Gridap
@@ -73,117 +73,65 @@ R(ϕ,θ,ψ) = Rx(ϕ)⋅Ry(θ)⋅Rz(ψ)
 R(θ) = R(θ,θ,θ)
 
 
-#X,T,N = read_stl(joinpath(@__DIR__,"data/Bunny-LowPoly.stl"))
-#X,T,N = read_stl(joinpath(@__DIR__,"data/wine_glass.stl"))
-X,T,N = read_stl(joinpath(@__DIR__,"data/cube.stl"))
 
-stl0 = compute_stl_model(T,X)
-stl0 = merge_nodes(stl0)
+geometries = [ "cube", "Bunny-LowPoly", "wine_glass" ]
+size_factors = [1, 2] #, 4, 8 ]
 
-X0 = get_node_coordinates(get_grid(stl0))
-T0 = get_cell_nodes(stl0)
+base_sizes = [ 5, 10, 20 ]
+ref_volumes = [ 1, 273280.0337419614, 74.12595970063474 ]
 
-## Coindident Grid-Cube
-δ = 0.2
-n = 7
-D = 3
-
-pmin,pmax = get_bounding_box(stl0)
-O = (pmin+pmax)/2
-
-Δ = (pmax-pmin)*δ
-pmin = pmin - Δ
-pmax = pmax + Δ
-partition = (n,n,n)
-
-grid = CartesianGrid(pmin,pmax,partition)
-
-
-θs = [0;exp10.( -17:0 )]
-Δx = VectorValue(0.0,0.0,0.1)
-
-for θ in θs
-  println("Testing θz = $θ ...")
-  Oi = O + Δx
-  Xi = map(p-> p + Δx,X0)
-  Xi = map(p-> Oi + Rz(θ)⋅(p-Oi),Xi)
-  stl = compute_stl_model(Table(T0),Xi)
-  #writevtk(stl.grid,"stl")
-  test_stl_cut(grid,stl,1)
-end
-
-## Rational Grid
-δ = 0.2
-n = 7
-D = 3
-
-pmin,pmax = get_bounding_box(stl0)
-pmin = Point(rationalize.(Tuple(pmin)))
-pmax = Point(rationalize.(Tuple(pmax)))
-δ = rationalize(δ)
-
-O = (pmin+pmax)/2
-
-Δ = (pmax-pmin)*δ
-pmin = pmin - Δ
-pmax = pmax + Δ
-partition = (n,n,n)
-
-grid = CartesianGrid(pmin,pmax,partition)
-
-δx = Point(0,0,1)
-Δxs = [0;exp10.(-17:-1)]
-
-for Δx in Δxs
-  println("Testing Δx = $Δx ...")
-  Xi = map(p-> p + Δx,X0)
-  stl = compute_stl_model(Table(T0),Xi)
-  writevtk(stl.grid,"stl")
-  test_stl_cut(grid,stl,1)
-end
-
-δ = 0.2
-n = 7
-D = 3
-
-pmin,pmax = get_bounding_box(stl0)
-pmin = Point(rationalize.(Tuple(pmin)))
-pmax = Point(rationalize.(Tuple(pmax)))
-δ = rationalize(δ)
-
-origins = [pmin, pmax, (pmin+pmax)/2 ]
-
-Δ = (pmax-pmin)*δ
-pmin = pmin - Δ
-pmax = pmax + Δ
-partition = (n,n,n)
-
-grid = CartesianGrid(pmin,pmax,partition)
+M = CartesianIndices( (length(geometries), length(size_factors) ) ) 
 
 θs = [0;exp10.( -17:-1 )]
+Δxs = [0;exp10.(-17:-1)]
 
-O = origins[3]
-θ = 1e-16
-Xi = map(p-> O + R(θ)⋅(p-O),X0)
-stl = compute_stl_model(Table(T0),Xi)
-test_stl_cut(grid,stl,1)
+δ = 0.2
 
-for O in origins
+for I in M
+  geometry = geometries[ I[1] ]
+  size_factor = size_factors[ I[2] ]
+
+  base_size = base_sizes[ I[1] ]
+  ref_volume = ref_volumes[ I[1] ]
+
+  n = size_factor * base_size
+
+  X,T,N = read_stl(joinpath(@__DIR__,"data/$geometry.stl"))
+
+  stl0 = compute_stl_model(T,X)
+  stl0 = merge_nodes(stl0)
+
+  X0 = get_node_coordinates(get_grid(stl0))
+  T0 = get_cell_nodes(stl0)
+
+  pmin,pmax = get_bounding_box(stl0)
+  O = (pmin+pmax)/2
+
+  Δ = (pmax-pmin)*δ
+  pmin = pmin - Δ
+  pmax = pmax + Δ
+  partition = (n,n,n)
+
+  grid = CartesianGrid(pmin,pmax,partition)
+
+  println("Testing `$geometry.stl` with `mesh = ($n×$n×$n)` :")
+
   for θ in θs
-    println("Testing θ = $θ over $O ...")
+    println("Testing θz = $θ ...")
     Xi = map(p-> O + R(θ)⋅(p-O),X0)
     stl = compute_stl_model(Table(T0),Xi)
-    writevtk(stl,"stl")
-    test_stl_cut(grid,stl,1)
+    #writevtk(stl.grid,"stl")
+    test_stl_cut(grid,stl,ref_volumes[ I[1] ])
   end
+
+  for Δx in Δxs
+    println("Testing Δx = $Δx ...")
+    Xi = map(p-> p + Δx,X0)
+    stl = compute_stl_model(Table(T0),Xi)
+    #writevtk(stl.grid,"stl")
+    test_stl_cut(grid,stl,ref_volumes[ I[1] ])
+  end
+
 end
 
-
-
-# TODO:
-#  * test exact matching with rational CartesianGrid
-#  * Test more displacements
-#  * rotate in more axis ϕ,θ,ψ (x,y,z) ( roll, pitch, yaw)
-#  * more geometries
-#  * more displacements
 end # module
