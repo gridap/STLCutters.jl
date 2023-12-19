5e45f0d1b4217970c8b22cd091613c5be9115734
janmodderman <janmodderman@gmail.com>
2023-12-19 11:09:52 +0100

    added test for cutgeo_facets

---
+++ test/Cutgeo_facetsTest.jl
@@ -0,0 +1,104 @@
+module cutgeo_facetstest
+using Gridap
+using GridapEmbedded
+using STLCutters
+using Test
+
+geo = STLGeometry(joinpath(@__DIR__,"data/cube.stl"))
+geoₐ = cube(x0=Point(0.0,0.0,0.0))
+
+pmin = Point(-1.0, -1.0, 0.5)
+pmax = Point(1.0, 1.0, 1.5)
+n = 5
+nz = 3
+partition = (n,n,nz)
+
+model = CartesianDiscreteModel(pmin, pmax, partition)
+cutgeo,bgf_to_ioc,cutgeo_facets = cut_facets(model, geo)
+cutgeo_facetsₐ = cut_facets(model, geoₐ)
+
+labels = get_face_labeling(model)
+add_tag_from_tags!(labels,"top",[21])
+
+Γbin = BoundaryTriangulation(cutgeo_facets,CUT_IN)
+Γbout = BoundaryTriangulation(cutgeo_facets,CUT_OUT)
+Λin =  SkeletonTriangulation(cutgeo_facets,CUT_IN)
+Λout = SkeletonTriangulation(cutgeo_facets,CUT_OUT)
+
+Γbinₐ = BoundaryTriangulation(cutgeo_facetsₐ,CUT_IN)
+Γboutₐ = BoundaryTriangulation(cutgeo_facetsₐ,CUT_OUT)
+Λinₐ =  SkeletonTriangulation(cutgeo_facetsₐ,CUT_IN)
+Λoutₐ = SkeletonTriangulation(cutgeo_facetsₐ,CUT_OUT)
+
+Γ = EmbeddedBoundary(cutgeo)
+Γt = BoundaryTriangulation(model,tags=["top"])
+
+Γ⁺_act = BoundaryTriangulation(cutgeo_facets,ACTIVE_OUT,tags=["top"])
+Γ⁺_cut = BoundaryTriangulation(cutgeo_facets,CUT_OUT,tags=["top"])
+Γ⁺_phy = BoundaryTriangulation(cutgeo_facets,PHYSICAL_OUT,tags=["top"])
+
+Γ⁻_act = BoundaryTriangulation(cutgeo_facets,ACTIVE_IN,tags=["top"])
+Γ⁻_cut = BoundaryTriangulation(cutgeo_facets,CUT_IN,tags=["top"])
+Γ⁻_phy = BoundaryTriangulation(cutgeo_facets,PHYSICAL_IN,tags=["top"])
+
+Γ⁺_actₐ = BoundaryTriangulation(cutgeo_facetsₐ,ACTIVE_OUT,tags=["top"])
+Γ⁺_cutₐ = BoundaryTriangulation(cutgeo_facetsₐ,CUT_OUT,tags=["top"])
+Γ⁺_phyₐ = BoundaryTriangulation(cutgeo_facetsₐ,PHYSICAL_OUT,tags=["top"])
+
+Γ⁻_actₐ = BoundaryTriangulation(cutgeo_facetsₐ,ACTIVE_IN,tags=["top"])
+Γ⁻_cutₐ = BoundaryTriangulation(cutgeo_facetsₐ,CUT_IN,tags=["top"])
+Γ⁻_phyₐ = BoundaryTriangulation(cutgeo_facetsₐ,PHYSICAL_IN,tags=["top"])
+
+degree = 2
+dΓ = Measure(Γ,degree)
+dΓt = Measure(Γt,degree)
+
+dΓ⁺_act = Measure(Γ⁺_act,degree)
+dΓ⁺_cut = Measure(Γ⁺_cut,degree)
+dΓ⁺_phy = Measure(Γ⁺_phy,degree)
+dΓ⁻_act = Measure(Γ⁻_act,degree)
+dΓ⁻_cut = Measure(Γ⁻_cut,degree)
+dΓ⁻_phy = Measure(Γ⁻_phy,degree)
+
+dΓ⁺_actₐ = Measure(Γ⁺_actₐ,degree)
+dΓ⁺_cutₐ = Measure(Γ⁺_cutₐ,degree)
+dΓ⁺_phyₐ = Measure(Γ⁺_phyₐ,degree)
+dΓ⁻_actₐ = Measure(Γ⁻_actₐ,degree)
+dΓ⁻_cutₐ = Measure(Γ⁻_cutₐ,degree)
+dΓ⁻_phyₐ = Measure(Γ⁻_phyₐ,degree)
+
+dΓbin = Measure(Γbin,degree)
+dΓbout = Measure(Γbout,degree)
+dΛin = Measure(Λin,degree)
+dΛout = Measure(Λout,degree)
+
+dΓbinₐ = Measure(Γbinₐ,degree)
+dΓboutₐ = Measure(Γboutₐ,degree)
+dΛinₐ = Measure(Λinₐ,degree)
+dΛoutₐ = Measure(Λoutₐ,degree)
+
+# test area vs. known value
+@test sum(∫(1)dΓ⁺_phy) + sum(∫(1)dΓ⁻_phy) ≈ 4
+@test sum(∫(1)dΓ⁺_act) + sum(∫(1)dΓ⁻_act) - sum(∫(1)dΓ⁺_cut) - sum(∫(1)dΓ⁻_cut)  ≈ 4
+@test sum(∫(1)dΓ⁺_phy) ≈ 3
+@test sum(∫(1)dΓ⁺_act) - sum(∫(1)dΓ⁻_cut) ≈ 3
+@test sum(∫(1)dΓ⁻_phy) ≈ 1
+@test sum(∫(1)dΓ⁻_act) - sum(∫(1)dΓ⁺_cut) ≈ 1
+
+# test area contributions vs. total value
+@test sum(∫(1)dΓ⁺_phy) + sum(∫(1)dΓ⁻_phy) ≈ sum(∫(1)dΓt)
+@test sum(∫(1)dΓ⁺_act) + sum(∫(1)dΓ⁻_act) - sum(∫(1)dΓ⁺_cut) - sum(∫(1)dΓ⁻_cut)  ≈ sum(∫(1)dΓt)
+
+# test STLCutter vs GridapEmbedded
+@test sum(∫(1)dΓ⁺_phy) ≈ sum(∫(1)dΓ⁺_phyₐ)
+@test sum(∫(1)dΓ⁺_act) ≈ sum(∫(1)dΓ⁺_actₐ)
+@test sum(∫(1)dΓ⁺_cut) ≈ sum(∫(1)dΓ⁺_cutₐ)
+@test sum(∫(1)dΓ⁻_phy) ≈ sum(∫(1)dΓ⁻_phyₐ)
+@test sum(∫(1)dΓ⁻_phy) ≈ sum(∫(1)dΓ⁻_phyₐ)
+@test sum(∫(1)dΓ⁻_phy) ≈ sum(∫(1)dΓ⁻_phyₐ)
+@test sum(∫(1)dΓbin) ≈ sum(∫(1)dΓbinₐ)
+@test sum(∫(1)dΓbout) ≈ sum(∫(1)dΓboutₐ)
+# @test sum(∫(1)dΛin) ≈ sum(∫(1)dΛinₐ)
+# @test sum(∫(1)dΛout) ≈ sum(∫(1)dΛoutₐ)
+
+end # module
